114b78746f3d56243800db354ccaa26f019f2e5b
cece80 <ramokgadi.ks@gmail.com>
2026-05-03 23:18:55 +0200

    feat: add auth barrel export

---
+++ src/lib/auth/index.ts
@@ -0,0 +1,6 @@
+// src/lib/auth/index.ts
+export * from "./auth-provider";
+export * from "./registration";
+export * from "./profile-switcher";
+export * from "./permissions";
+export * from "./cloud-sync";
